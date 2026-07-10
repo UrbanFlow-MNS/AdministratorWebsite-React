11206b894520f51f0b73e88dfad1f8b2318acf8f
mathiasv2 <dumas.mathias@outlook.com>
2026-07-10 14:55:28 +0200

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -6,6 +8,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
-  ]
+    tailwindcss(),
+  ],
+  test: {
+    coverage: {
+      reporter: ['text', 'lcov']
+    }
+  }
 })
